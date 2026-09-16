3173f647ebccf49196c55197349db148f158d5ae
AlanYan <AYwlilwYA@users.noreply.github.com>
2026-09-16 12:16:14 +0800

    版本号同步为 0.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = 1
-        versionName = "0.0.1-framework"
+        versionName = "0.1.0"
     }
 
     buildTypes {
